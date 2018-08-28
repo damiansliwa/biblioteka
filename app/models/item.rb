1224ed3048e41e5c25ca9de91f533ea677a290a9
damiansliwa <damian.sliwa.90@gmail.com>
2018-08-28 21:28:11 +0200

    Add validations for name presence and ISBN length

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,5 @@
 class Item < ApplicationRecord
 	# belongs_to :user
+	validates :name, presence: true, length: { maximum: 150 }
+	validates :ISBN, length: { is: 17 }, allow_blank: true, allow_nil: true
 end
