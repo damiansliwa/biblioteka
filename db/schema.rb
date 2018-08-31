c102bf24cd3145337f0143e6e6732adfa92f7755
damiansliwa <damian.sliwa.90@gmail.com>
2018-08-31 20:56:01 +0200

    Generate migration which adds user_id to items

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_28_145739) do
+ActiveRecord::Schema.define(version: 2018_08_31_185310) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 2018_08_28_145739) do
     t.integer "item_type", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
