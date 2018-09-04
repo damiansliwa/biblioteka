53e90978e29ce1358f88649bb49c7ad95edf910b
damiansliwa <damian.sliwa.90@gmail.com>
2018-09-04 19:49:37 +0200

    Create add_index_to_user_id migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_31_185310) do
+ActiveRecord::Schema.define(version: 2018_09_04_174305) do
 
   create_table "items", force: :cascade do |t|
     t.string "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 2018_08_31_185310) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.index ["user_id"], name: "index_items_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
