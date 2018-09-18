764cdb6d3fb8547522b1de296f8d056bfbbdeff7
damiansliwa <damian.sliwa.90@gmail.com>
2018-09-18 14:35:21 +0200

    Improve search form tests

--- spec/features/items_search_forms_spec.rb
+++ spec/features/items_search_forms_spec.rb
@@ -4,30 +4,33 @@ RSpec.feature "ItemsSearchForms", type: :feature do
   
   context "when search" do
   	before do
-  	  item = Item.create!(name: "BookName", author: "BookAuthor")
+  	  item1 = Item.create!(name: "BookName1", author: "BookAuthor1")
+  	  item2 = Item.create!(name: "BookName2", author: "BookAuthor2")
   	end
 
   	it "shows the 'Searching results' header" do
   	  visit '/items'
   	  fill_in 'search', with: 'Lorem ipsum'
-  	  click_on 'Search'
+  	  click_button 'Search'
   	  expect(page).to have_content('Searching results:')
   	end
 
-  	it "search items by its name" do
+  	it "search items by item name" do
   	  visit '/items'
-  	  fill_in 'search', with: 'BookName'
-  	  click_on 'Search'
+  	  fill_in 'search', with: 'BookName1'
+  	  click_button 'Search'
   	  expect(page).to have_content('Searching results:')
-      expect(page).to have_content('BookName')
+      expect(page).to have_content('BookName1')
+      expect(page).to_not have_content('BookName2')
   	end
 
-  	it "search items by its author" do
+  	it "search items by item author" do
   	  visit '/items'
-  	  fill_in 'search', with: 'BookAuthor'
-  	  click_on 'Search'
+  	  fill_in 'search', with: 'BookAuthor1'
+  	  click_button 'Search'
   	  expect(page).to have_content('Searching results:')
-      expect(page).to have_content('BookAuthor')
+      expect(page).to have_content('BookAuthor1')
+      expect(page).to_not have_content('BookAuthor2')
   	end
   end
 end
